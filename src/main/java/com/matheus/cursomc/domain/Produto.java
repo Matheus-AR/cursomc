8d0548c4bc996ea8e8d3eb4d877b4928c3d7b73f
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-07-09 22:31:52 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/matheus/cursomc/domain/Produto.java
+++ src/main/java/com/matheus/cursomc/domain/Produto.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,6 +36,7 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -48,6 +50,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
 		for (ItemPedido x : itens) {
